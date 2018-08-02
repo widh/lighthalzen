8aa4baeb0324b7e7176c006ef281627f0501170d
Yuoa <contact@yuoa.pm>
2018-08-02 13:01:23 +0900

    Menu translation test (1/2)

---
+++ i18n/menu_catalog.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Translation catalog for code-unquoted menu.
+ *
+ * This file includes menu strings for easy translation in POEDIT.
+ *
+ * @todo More non-complex way?
+ *
+ * @package Lighthalzen
+ * @subpackage Rekenber
+ *
+ * @link https://github.com/yuoa/lighthalzen#readme
+ * @author Jio Gim (CiTE 5th)
+ * @copyright Yuoa (Jio Gim)
+ *
+**/
+
+    _t("CiTE");
+    _t("주임교수 인사말");
+    _t("ICT명품인재양성사업");
+    _t("교육 철학과 체계");
+    _t("협력기관");
+    _t("조직도");
+    _t("찾아오시는 길");
+    _t("홍보 자료");
+
+    _t("구성원 소개");
+    _t("교수");
+    _t("전임 연구원");
+    _t("직원");
+
+    _t("학과 소식");
+    _t("공지사항");
+    _t("입학정보");
+    _t("채용정보");
+    _t("세미나 및 행사");
+    _t("뉴스레터");
+    _t("갤러리");
+    _t("학과 자료실");
+
+    _t("연구");
+    _t("연구 분야");
+    _t("연구 실적");
+    _t("연구장비 현황");
+    _t("대여가능 연구장비");
+
+?>
