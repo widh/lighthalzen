d0e7233f9dd9513b042fcfd4ad96f60e65912fb1
Yuoa <contact@yuoa.pm>
2018-08-08 06:37:12 +0900

    Top slider implemented

--- index.php
+++ index.php
@@ -45,8 +45,8 @@
         <article>
             <div>
                 <section>
-                    <h1>개발자는 뭘 하고 있는가 (실시간)</h1>
-                    <p><em><b><h3>슬라이더 제작중</h3></b></em></p>
+                    <h1>현재 작업 부분</h1>
+                    <p><em><b><h3>메인 슬라이더</h3></b></em></p>
                 </section>
                 <section>
                     <h1>개발 환경과 대응 환경</h1>
@@ -98,54 +98,38 @@
                     <h1>08. 07. (화) 목표</h1>
                     <ol>
                         <li><s>최상단 바로가기 버튼 구현</s> 완료</li>
-                        <li><s>상단 메뉴 margin/top 수식이 어느 환경에서도 성립하는지 재확인</s></li>
+                        <li><s>상단 메뉴 margin/top 수식이 어느 환경에서도 성립하는지 재확인</s> 완료</li>
                         <li><s>모바일 메뉴 구현</s> 완료</li>
-                        <li>슬라이더 구현</li>
+                        <li><s>슬라이더 구현</s> 완료!! 이걸 css만으로 구현하게 될 줄이야</li>
                     </ol>
                 </section>
                 <section>
                     <h1> 08. 08. (수) 목표</h1>
                     <ol>
+                        <li>슬라이더 컨트롤러 구현</li>
                         <li>푸터 완성</li>
-                        <li>검색 페이지 완성</li>
-                        <li>404 페이지 제작</li>
                         <li>모두 펼치기 기능 완성</li>
                         <li>브라우저 호환성 중간점검 및 대응</li>
+                        <li>슬라이더 포스터 <code>max-height</code> 부분 재점검</li>
                     </ol>
                 </section>
                 <section>
                     <h1>08. 09. (목) 목표</h1>
                     <ol>
+                        <li>메인 메뉴에 &lt;title&gt; 어트리뷰트 메뉴 적용</li>
                         <li>메뉴 받아서 세부 페이지 및 메인 페이지 동시 개발 시작</li>
+                        <li>학과 로고 언어/플로팅 각각의 이미지 적용</li>
+                        <li>검색 페이지 완성</li>
+                        <li>404 페이지 제작</li>
                     </ol>
                 </section>
                 <section>
-                    <h1>헤더 개발</h1>
-                    <p><s>1. 상단 메뉴 드롭다운 디자인</s> 완료</p>
-                    <p>2. 모두 펼치기 기능 구현</p>
-                    <p>3. 버튼 속 메뉴 구현</p>
-                    <p><s>4. 플로팅 메뉴 구현</s> 완료</p>
-                    <p>5. 반응형 테스트</p>
-                    <p><s>6. 초기 로드 시 Transition Off</s> 완료</p>
-                    <p><s>7. 버튼 tabindex 취소</s> 완료</p>
-                    <p>8. 학과 로고 언어/플로팅 각각의 이미지 적용</p>
-                    <p><s>9. 플로팅 시 메뉴 디자인 변경</s> 완료</p>
-                    <p>10. 슬라이더 구현</p>
-                    <p><s>11. 언어 버튼 디자인 재고려</s> 완료</p>
-                    <p><s>12. em => rem 단위 변경 및 재계산</s> 완료</p>
-                    <p><s>13. 복잡한 calc 계산식 구조화 및 단순화 혹은 비-calc 크기로 교체</s> 완료</p>
-                    <p><s>14. scss 구조 재설계</s> 완료</p>
-                    <p>15. max-content 속성의 구버전 브라우저를 위한 추가 JS 적용</p>
-                    <p><s>16. 플로팅 바 transition 추가</s> 완료</p>
-                    <p><s>17. 디자인 리마스터</s> 완료</p>
-                    <p><s>18. 상단 메뉴 radius 추가</s> 완료</p>
-                    <p><s>19. focus에의 브라우저 css 덮어쓰기</s> 완료</p>
-                    <p>20. 최상단 바로가기 버튼 추가</p>
-                    <p>21. &lt;title&gt; 어트리뷰트 메뉴 적용</p>
-                </section>
-                <section>
-                    <h1>푸터 개발</h1>
-                    <p>1차 개발중</p>
+                    <h1>알려진 호환성 문제</h1>
+                    <ul>
+                        <li>CSS <code>width: max-content</code> 속성</li>
+                        <li>CSS <code>display: contents</code> 속성</li>
+                        <li>JS <code>window.scrollY</code></li>
+                    </ul>
                 </section>
             </div>
         </article>
